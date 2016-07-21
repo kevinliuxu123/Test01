52cb3aa5f26e51a482642f8dc0505f0e79f051fe
kevinliuxu123 <421495793@qq.com>
2016-07-21 13:59:09 +0800

    1

--- test/src/test/test1.java
+++ test/src/test/test1.java
@@ -5,28 +5,4 @@ public class test1 {
 
 	}
 	
-	public void method02() {
-
-
-	}
-	public void method03() {
-
-
-	}
-	public void method04() {
-
-
-	}
-	public void method05() {
-
-
-	}
-	public void method06() {
-
-
-	}
-	public void method07() {
-
-
-	}
 }
